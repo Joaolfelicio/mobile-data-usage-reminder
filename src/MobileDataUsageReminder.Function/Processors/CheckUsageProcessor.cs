d25a159bd39c198ad072c4d445f2da077ca146f4
Joaolfelicio <joaolfelicio@gmail.com>
2022-04-13 22:55:58 +0200

    Start working on webhook event dispatcher

---
+++ src/MobileDataUsageReminder.Function/Processors/CheckUsageProcessor.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+
+public class CheckUsageProcessor : ICheckUsageProcessor
+{
+    public CheckUsageProcessor(Parameters)
+    {
+        
+    }
+
+    public async Task ProcessCommand(EventPayload eventPayload)
+    {
+        throw new System.NotImplementedException();
+    }
+}
